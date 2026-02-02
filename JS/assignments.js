964a4ab719f4d3d84ba9c00b3bca1e0cca90aaac
Oskar Berntson <oskar.berntson@gmail.com>
2026-02-02 14:19:32 +0100

    Pathway experiment

--- JS/assignments.js
+++ JS/assignments.js
@@ -2,14 +2,14 @@ export const assignments = [
     {
         id: "assignment1",
         title: "Uppgift 1 - Samlingssida för inlämningsuppgifter",
-        link: "1me323-assignments//assignment1/index.html",
+        link: "1me323-assignments/assignment1/index.html",
         description: "I denna uppgift ska du skapa ett nytt repository för alla inlämningsuppgifter i kursen och publicera det på GitHub Pages."
     },
 
     {
         id: "assignment2",
         title: "Uppgift 2 - Produktsida med kundvagn",
-        link: "1me323-assignments//assignment2/index.html",
+        link: "1me323-assignments/assignment2/index.html",
         description: "I denna uppgift ska du skapa en produktsida som dynamiskt renderar produkter från en datastruktur."
     }
 ]
