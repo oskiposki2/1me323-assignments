c2afbfacb586e028a0d49d75148b1a55c815b0dd
Oskar Berntson <oskar.berntson@gmail.com>
2026-02-03 15:22:32 +0100

    Fixing Pathway for GitHub

--- JS/assignments.js
+++ JS/assignments.js
@@ -2,7 +2,7 @@ export const assignments = [
     {
         id: "assignment1",
         title: "Uppgift 1 - Samlingssida för inlämningsuppgifter",
-        link: "/1me323-assignments/assignment2/index.html",
+        link: "/1me323-assignments/assignment1/index.html",
         description: "I denna uppgift ska du skapa ett nytt repository för alla inlämningsuppgifter i kursen och publicera det på GitHub Pages."
     },
 
